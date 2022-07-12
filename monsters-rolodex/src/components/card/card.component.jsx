58bdaceffd933cc84ee1b930b3796efa2357a915
Jason Doze <jasondoze@jd.ht.home>
2022-07-12 17:37:41 -0500

    convert all class components in monstrosities to functional components using useEffect with hooks

--- monsters-rolodex/src/components/card/card.component.jsx
+++ monsters-rolodex/src/components/card/card.component.jsx
@@ -1,20 +1,39 @@
-import { Component } from "react";
+// import { Component } from "react";
+
 import "./card.styles.css";
 
-class Card extends Component {
-  render() {
-    const { id, name, email } = this.props.monster;
-    return (
-      <div className="card-container" key={id}>
-        <img
-          alt={`monster ${name}`}
-          src={`https://robohash.org/${id}?set=set2size=180x180`}
-        />
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
-    );
-  }
-}
+// functional component
+const Card = ({ monster }) => {
+  const { id, name, email } = monster;
+  return (
+    <div className="card-container" key={id}>
+      <img
+        alt={`monster ${name}`}
+        src={`https://robohash.org/${id}?set=set2size=180x180`}
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
 
 export default Card;
+
+// class Component
+// class Card extends Component {
+//   render() {
+//     const { id, name, email } = this.props.monster;
+//     return (
+//       <div className="card-container" key={id}>
+//         <img
+//           alt={`monster ${name}`}
+//           src={`https://robohash.org/${id}?set=set2size=180x180`}
+//         />
+//         <h2>{name}</h2>
+//         <p>{email}</p>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Card;
--- monsters-rolodex/src/components/search-box/search-box.component.jsx
+++ monsters-rolodex/src/components/search-box/search-box.component.jsx
@@ -1,19 +1,33 @@
-import { Component } from "react";
+// import { Component } from "react";
 import "./search-box.styles.css";
 
-class SearchBox extends Component {
-  render() {
-    return (
-      <input
-        className={this.props.className}
-        // gererate a search field with an x to clear the field
-        type="search"
-        placeholder={this.props.placeholder}
-        // we changed this to not re-run everytime its intialized
-        onChange={this.props.onChangeHandler}
-      />
-    );
-  }
-}
+const SearchBox = ({ className, placeholder, onChangeHandler }) => (
+  <input
+    className={className}
+    // gererate a search field with an x to clear the field
+    type="search"
+    placeholder={placeholder}
+    // we changed this to not re-run everytime its intialized
+    onChange={onChangeHandler}
+  />
+);
 
 export default SearchBox;
+
+// class Component
+// class SearchBox extends Component {
+//   render() {
+//     return (
+//       <input
+//         className={this.props.className}
+//         // gererate a search field with an x to clear the field
+//         type="search"
+//         placeholder={this.props.placeholder}
+//         // we changed this to not re-run everytime its intialized
+//         onChange={this.props.onChangeHandler}
+//       />
+//     );
+//   }
+// }
+
+// export default SearchBox;
