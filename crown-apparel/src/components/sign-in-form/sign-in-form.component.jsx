83c3e5821df87286a17089bdc1d007780fc87d77
Jason Doze <jdoze@protonmail.com>
2022-07-27 17:51:15 -0500

    add authentication page component with sign in for

--- crown-apparel/src/components/sign-in-form/sign-in-form.component.jsx
+++ crown-apparel/src/components/sign-in-form/sign-in-form.component.jsx
@@ -37,7 +37,18 @@ const SignInForm = () => {
       );
       console.log(response);
       resetFormFields();
-    } catch (error) {}
+    } catch (error) {
+      switch (error.code) {
+        case 'auth/wrong-password':
+          alert('incorrect password for email');
+          break;
+        case 'auth/user-not-found':
+          alert('no user associated with this email');
+          break;
+        default:
+          console.log(error);
+      }
+    }
   };
 
   // on changehandler for each event
@@ -71,9 +82,7 @@ const SignInForm = () => {
         />
         <div className='buttons-container'>
           <Button type='submit'>Sign In</Button>
-          <Button buttonType='google' onClick={signInWithGoogle}>
-            Google sign in
-          </Button>
+          <Button type='button' buttonType='google' onClick={signInWithGoogle}>Google Sign In</Button>
         </div>
       </form>
     </div>
