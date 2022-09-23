ed05508758647e130256f497a7d69c1a4c988432
Jason Doze <jdoze@protonmail.com>
2022-09-22 21:56:41 -0500

    Added catagory directory with reducers/ selectors for redux state changes.
    Signed-off-by: Jason Doze <jdoze@protonmail.com>

--- crown-apparel/src/contexts/categories.context.jsx
+++ crown-apparel/src/contexts/categories.context.jsx
@@ -1,27 +1,27 @@
-import { createContext, useState, useEffect } from 'react';
+// import { createContext, useState, useEffect } from 'react';
 
-import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+// import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
 
-export const CategoriesContext = createContext({
-  categoriesMap: {},
-});
+// export const CategoriesContext = createContext({
+//   categoriesMap: {},
+// });
 
-export const CategoriesProvider = ({ children }) => {
-  const [categoriesMap, setCategoriesMap] = useState({});
+// export const CategoriesProvider = ({ children }) => {
+//   const [categoriesMap, setCategoriesMap] = useState({});
 
-  useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments('categories');
-      setCategoriesMap(categoryMap);
-    };
+//   useEffect(() => {
+//     const getCategoriesMap = async () => {
+//       const categoryMap = await getCategoriesAndDocuments('categories');
+//       setCategoriesMap(categoryMap);
+//     };
 
-    getCategoriesMap();
-  }, []);
+//     getCategoriesMap();
+//   }, []);
 
-  const value = { categoriesMap };
-  return (
-    <CategoriesContext.Provider value={value}>
-      {children}
-    </CategoriesContext.Provider>
-  );
-};
+//   const value = { categoriesMap };
+//   return (
+//     <CategoriesContext.Provider value={value}>
+//       {children}
+//     </CategoriesContext.Provider>
+//   );
+// };
