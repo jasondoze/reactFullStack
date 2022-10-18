179963b7817eb57c24df9dbc566c2c0c0ac3d64d
Jason Doze <dozejason@gmail.com>
2022-10-17 19:26:35 -0500

    Add thunk for async redux.
    Signed-off-by: Jason Doze <dozejason@gmail.com>

--- crown-apparel/src/routes/shop/shop.component.jsx
+++ crown-apparel/src/routes/shop/shop.component.jsx
@@ -2,24 +2,16 @@ import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-
-import { setCategories } from '../../store/categories/category.action';
-
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
+
+import { fetchCategoriesAsync } from '../../store/categories/category.action';
 
 const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoriesArray = await getCategoriesAndDocuments('categories');
-      console.log(categoriesArray);
-      dispatch(setCategories(categoriesArray));
-    };
-
-    getCategoriesMap();
+    dispatch(fetchCategoriesAsync());
   }, [dispatch]);
 
   return (
--- crown-apparel/src/store/store.js
+++ crown-apparel/src/store/store.js
@@ -1,24 +1,35 @@
 import { compose, createStore, applyMiddleware } from 'redux';
-// import logger from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk'
 
 import { rootReducer } from './root-reducer';
 
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-
-  console.log('type: ', action.type);
-  console.log('payload: ', action.payload);
-  console.log('currentState: ', store.getState());
-
-  next(action);
-
-  console.log('next state: ', store.getState());
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
 };
 
-const middleWares = [loggerMiddleware];
+const persitedReducer = persistReducer(persistConfig, rootReducer);
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+// hide loggers while in production environment
+const middleWares = [
+  process.env.NODE_ENV === 'development' && logger,
+  thunk,
+].filter(Boolean);
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+// if we are not in production and there is no window and these chrome dev tools exist, use dev tools
+// else just use redux compose
+const composedEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composedEnhancer(applyMiddleware(...middleWares));
+
+export const store = createStore(persitedReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store);
