0d0e50c27dc516c34d9f5a988ea2ac5173165240
Jason Doze <dozejason@gmail.com>
2022-10-19 21:39:57 -0500

    Add stripe payment system to project.
    Signed-off-by: Jason Doze <dozejason@gmail.com>

--- crown-apparel/src/components/button/button.component.jsx
+++ crown-apparel/src/components/button/button.component.jsx
@@ -1,15 +1,8 @@
-/*
-default button
-
-inverted button
-
-google signin button
-*/
-
 import {
   BaseButton,
   GoogleSignInButton,
   InvertedButton,
+  ButtonSpinner,
 } from './button.styles';
 
 export const BUTTON_TYPE_CLASSES = {
@@ -25,9 +18,13 @@ const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
     [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
   }[buttonType]);
 
-const Button = ({ children, buttonType, ...otherProps }) => {
+const Button = ({ children, buttonType, isLoading, ...otherProps }) => {
   const CustomButton = getButton(buttonType);
-  return <CustomButton {...otherProps}>{children}</CustomButton>;
+  return (
+    <CustomButton disabled={isLoading} {...otherProps}>
+      {isLoading ? <ButtonSpinner /> : children}
+    </CustomButton>
+  );
 };
 
 export default Button;
